00cd71c8795129f225ed3043f96829b89ea5e6b3
ankurg10 <ankurg@vt.edu>
2026-09-17 17:21:41 -0400

    Add files via upload

---
+++ evil.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+blob = r"""
+AAAN¼G.=W=bêøöo_ÌI(6ïÌ‹ú(&9:”8ªV©ğc^o@n²è?L)¹¬*Ñ×måû¶Xi^î|½FcÇ\Ë˜·+‡ZR_SA–ºrşæîö TÁ×ÅÍYYÇ1–âmı’¡HÑÏo®w¦®i"nEH¬{^;7£
+"""
+from hashlib import sha256
+
+h = sha256(blob.encode("latin-1")).hexdigest()
+
+if h == "46bf14dca6378f42fa071409021f07454bc91be6e93ffa04ee279ff3b5f447c2":
+    print("I come in peace.")
+else:
+    print("Prepare to be destroyed!")
---
+++ good.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+blob = r"""
+AAAN¼G.=W=bêøöo_ÌI(6oÌ‹ú(&9:”8ªV©ğc^o@n²è¿K)¹¬*Ñ×måû¶Xé^î|½FcÇ\Ë˜·+‡ZR_SA–ºrşfîö TÁ×ÅÍYYÇ1–âmı’¡HÑÏo.x¦®i"nEH¬{^š;7£
+"""
+from hashlib import sha256
+
+h = sha256(blob.encode("latin-1")).hexdigest()
+
+if h == "46bf14dca6378f42fa071409021f07454bc91be6e93ffa04ee279ff3b5f447c2":
+    print("I come in peace.")
+else:
+    print("Prepare to be destroyed!")
